92e69d80b7debffd0bda3f088a3f531e2caf7c1d
keshavaero0552 <keshav.aero0552@gmail.com>
2017-07-05 23:10:10 -0500

    keshav

---
+++ src/main/java/com/aop/SpringAopApplication.java
@@ -0,0 +1,21 @@
+package com.aop;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+
+
+	public class SpringAopApplication {  
+	    public static void main(String[] args){  
+	        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");  
+	        Operation e = (Operation) context.getBean("opBean");  
+	        System.out.println("calling msg...");  
+	        e.msg();  
+	        System.out.println("calling m...");  
+	        e.m();  
+	        System.out.println("calling k...");  
+	        e.k();  
+	    }  
+	}  
